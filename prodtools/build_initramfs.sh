6f1cd704ad25b347635332e6577443b03c70e3be
funtri-vt <funtri.vt@gmail.com>
2026-08-02 10:36:46 -0400

    fix(prodtools): minor syntax error

--- prodtools/build_initramfs.sh
+++ prodtools/build_initramfs.sh
@@ -198,7 +198,7 @@ sleep 5
 TARGET_DEV=""
 for i in $(seq 1 15); do
     TARGET_DEV=$(blkid | grep "DEB_ROOT" | head -n1 | cut -d: -f1)
-    if [ -n "$TARGET_DEV" ] then
+    if [ -n "$TARGET_DEV" ]; then
         echo "[AUTOMATON] FOund DEB_ROOT at: $TARGET_DEV"
     fi
     sleep 1
