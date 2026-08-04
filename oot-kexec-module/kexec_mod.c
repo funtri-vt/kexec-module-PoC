c258e7afe00c35e508267c5da15c17ed40f70b4b
funtri-vt <funtri.vt@gmail.com>
2026-08-04 19:51:17 +0000

    fix: change licenses in source files to gpl v2.0

--- oot-kexec-module/kexec_mod.c
+++ oot-kexec-module/kexec_mod.c
@@ -1,18 +1,21 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 //     kexec_mod.c: performs an in memory kexec through a misc device for kernels that don't support it
 //     Copyright (C) 2026  funtri-vt (funtri.vt@gmail.com)
 
-//     This program is free software: you can redistribute it and/or modify
-//     it under the terms of the GNU General Public License as published by
-//     the Free Software Foundation, either version 3 of the License, or
-//     (at your option) any later version.
-
-//     This program is distributed in the hope that it will be useful,
-//     but WITHOUT ANY WARRANTY; without even the implied warranty of
-//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//     GNU General Public License for more details.
-
-//     You should have received a copy of the GNU General Public License
-//     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 /* Auto-generated board configuration header */
 #include "board_config.h"
