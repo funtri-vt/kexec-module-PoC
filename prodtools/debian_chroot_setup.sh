92c3d3aa7444927c6e0a015bca8b1efc9433eb59
funtri-vt <funtri.vt@gmail.com>
2026-08-08 17:18:27 -0400

    fix: forgot to remove the old lines

--- prodtools/debian_chroot_setup.sh
+++ prodtools/debian_chroot_setup.sh
@@ -42,8 +42,6 @@ apt-get install -y \
     libfuse2 \
     libfuse3-*
 
-apt-get install -y cloud-utils zram-tools sudo command-not-found bash-completion libfuse2 libfuse3-*
-
 apt-get upgrade -y
 # Force initramfs-tools to include all firmware and drivers (crucial for cross-hardware builds)
 # sed -i 's/^MODULES=.*/MODULES=most/' /etc/initramfs-tools/initramfs.conf
